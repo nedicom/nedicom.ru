8d44e4f649a07be87f9b367fb84d8ec7d01a0187
Mark <m6132@yandex.ru>
2023-04-23 19:56:20 +0300

    еуые

--- resources/js/Pages/Nasledstvo.vue
+++ resources/js/Pages/Nasledstvo.vue
@@ -193,7 +193,7 @@ let submit = () => {
 
           <!-- practice -->   
 
-          <h1 class="my-6 py-6 text-4xl font-semibold text-grey text-center">Мои успехи говорят за себя</h1>
+          <h1 class="my-6 py-6 text-4xl font-semibold text-grey text-center">Делюсь успехами с Вами</h1>
 
 
 <div id="custom-controls-gallery" class="relative w-full" data-carousel="slide">
@@ -224,13 +224,13 @@ let submit = () => {
         <button type="button" class="flex justify-center items-center mr-4 h-full cursor-pointer group focus:outline-none" data-carousel-prev>
             <span class="text-gray-400 hover:text-gray-900 dark:hover:text-white group-focus:text-gray-900 dark:group-focus:text-white">
                 <svg aria-hidden="true" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd"></path></svg>
-                <span class="sr-only">Previous</span>
+                <span class="sr-only">Вперед</span>
             </span>
         </button>
         <button type="button" class="flex justify-center items-center h-full cursor-pointer group focus:outline-none" data-carousel-next>
             <span class="text-gray-400 hover:text-gray-900 dark:hover:text-white group-focus:text-gray-900 dark:group-focus:text-white">
                 <svg aria-hidden="true" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                <span class="sr-only">Next</span>
+                <span class="sr-only">Назад</span>
             </span>
         </button>
     </div>
