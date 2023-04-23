20d46fbb5adfc0c8a0947ab9f8024d2789ccca4c
Mark <m6132@yandex.ru>
2023-04-23 19:52:01 +0300

    sql connection

--- app/Http/Controllers/PostphoneController.php
+++ app/Http/Controllers/PostphoneController.php
@@ -8,11 +8,11 @@ use Illuminate\Http\Request;
 class PostphoneController extends Controller
 {
     public function postphone(Request $request){  
-
+        
                 $phone = $request->phone;
                 echo    $phone;             
-                $conn = mysqli_connect("127.0.0.1", "crm", "crm", "crm");
-
+                $conn = mysqli_connect("127.0.0.1", "developer", "Bonaqua123#$", "nedicom");
+                //$conn = mysqli_connect("127.0.0.1", "root", "", "crm");
                     if ($conn->connect_error) {
                     die("Connection failed: " . $conn->connect_error);
                     } 
@@ -21,6 +21,6 @@ class PostphoneController extends Controller
                 VALUES ('сайт наследство', 'без описания', $phone, 2, CURRENT_TIME(), 2, 'поступил', 5)"; //2 - Mark, 4 - Анастасия, 5 - иск, 67 - вера
                 $conn->query($sql);
 
-        return redirect()->route('Welcome');
+        return redirect()->route('login');
     }
 }
