5e97fc74e32abde97eb03fea1e55aaa832f73848
Mark <m6132@yandex.ru>
2023-06-13 13:48:18 +0300

    slider mobile

--- resources/js/Layouts/Slider.vue
+++ resources/js/Layouts/Slider.vue
@@ -11,12 +11,18 @@ defineProps({
   </h1>
   
   <div v-if="sldimg">
-    <Carousel :itemsToShow="3.95" :wrapAround="true" :transition="500">
+    <Carousel 
+      v-bind="settings" :breakpoints="breakpoints"
+      :wrapAround="true" 
+      :transition="500">
       <Slide v-for="slide in sldimg" :key="slide.practice_file_path">
-        <div class = "carousel__item w-full flex items-center justify-center h-128 bg-cover"
-        :style="{ backgroundImage: 'url(' + `/`+slide.practice_file_path + ')' }">
-            <a v-if="slide.url" :href= "'/articles/'+slide.url" 
-            class=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+        <div 
+          class = "carousel__item w-full flex items-center justify-center h-128 bg-cover"
+          :style="{ backgroundImage: 'url(' + `/`+slide.practice_file_path + ')' }">
+            <a 
+              v-if="slide.url" 
+              :href= "'/articles/'+slide.url" 
+              class=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
             подробнее</a>
         </div>
       </Slide>
@@ -53,13 +59,29 @@ defineProps({
   import 'vue3-carousel/dist/carousel.css'
   
   export default defineComponent({
-  name: 'Basic',
+  name: 'Breakpoints',
   components: {
     Carousel,
     Slide,
-    Pagination,
     Navigation,
+    Pagination,
   },
+  data: () => ({
+    // carousel settings
+    settings: {
+      itemsToShow: 1,
+      snapAlign: 'center',
+    },
+    // breakpoints are mobile first
+    // any settings not specified will fallback to the carousel settings
+    breakpoints: {
+      // 700px and up
+      1024: {
+        itemsToShow: 3.95,
+        snapAlign: 'center',
+      },
+    },
+  }),
 })
 </script>
 
