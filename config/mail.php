f8b762c4a378f0b5dd8c767c25441d87b91b53b3
Mark <m6132@yandex.ru>
2023-06-15 09:07:07 +0300

    'verify_peer' => env('MAIL_VERIFY_PEER', false),

--- config/mail.php
+++ config/mail.php
@@ -37,12 +37,13 @@ return [
         'smtp' => [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 25),
+            'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
+            'verify_peer' => env('MAIL_VERIFY_PEER', false),
         ],
 
         'ses' => [
