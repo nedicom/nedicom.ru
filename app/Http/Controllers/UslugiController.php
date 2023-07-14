549f3f4edc65315520ece846893d0fa51a8516c3
Mark <m6132@yandex.ru>
2023-07-14 10:54:08 +0300

    lawyer and banner on usluga

--- app/Http/Controllers/UslugiController.php
+++ app/Http/Controllers/UslugiController.php
@@ -8,6 +8,7 @@ use App\Helpers\Translate;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 
 class UslugiController extends Controller
@@ -21,9 +22,12 @@ class UslugiController extends Controller
     }
 
     public function show($url){ 
+        $id = Uslugi::where('url', '=', $url)->first()->id;
         return Inertia::render('Uslugi/Usluga', [
             'usluga' => Uslugi::where('url', '=', $url)->first(),
             'user' => Auth::user(),
+            'lawyers' => User::where('speciality_one_id', '=', $id)->orderBy('name', 'asc')->get()->take(3),
+            //'practice' => Article::where('userid', $id)->where('practice_file_path', '!=', null)->orderBy('updated_at', 'desc')->take(3)->get(),
             'firstlawyer' => Uslugi::where('url', '=', $url)->first()->firstlawyer,
 
         ]);
@@ -58,6 +62,13 @@ class UslugiController extends Controller
         $usluga = Uslugi::find($id);
         $usluga->usl_name = $request->header;
         $usluga->usl_desc = $request->description;
+        $usluga->longdescription = $request->longdescription;
+        $usluga->preimushestvo1 = $request->preimushestvo1;
+        $usluga->preimushestvo2 = $request->preimushestvo2;
+        $usluga->preimushestvo3 = $request->preimushestvo3;
+        $usluga->phone = $request->phone;
+        $usluga->address = $request->address;
+        $usluga->maps = $request->maps;
         $url = Translate::translit($request->header);        
         $usluga->url =  $url;
         $usluga->save();
--- resources/js/Pages/Uslugi/Usluga.vue
+++ resources/js/Pages/Uslugi/Usluga.vue
@@ -4,20 +4,23 @@ import Header from "@/Layouts/Header.vue";
 
 import Mainbanner from "@/Layouts/Mainbanner.vue";
 import Body from "@/Layouts/Body.vue";
-import Sidebaraction from "@/Layouts/Sidebaraction.vue";
+
+import Address from "@/Layouts/Address.vue";
 import MainFooter from "@/Layouts/MainFooter.vue";
-import Modal from '@/Components/Modal.vue';
-import { Head, Link } from "@inertiajs/inertia-vue3";
-import { ref } from "vue";
+import Slider from "@/Layouts/Slider.vue";
+import { Head, ref } from "@inertiajs/inertia-vue3";
 
 let vars = defineProps({
   usluga: String,
   user: String,
+  lawyers: String,
   firstlawyer: String,
+  practice: "$Array",
 });
 
+let sliderheader = "Доверяйте нашей практике";
 
-let mainbannerimg = ref("/storage/images/landing/main/1280on600.webp");
+let mainbannerimg = '/'+vars.lawyers[0].file_path;
 
 </script>
 
@@ -49,79 +52,46 @@ let mainbannerimg = ref("/storage/images/landing/main/1280on600.webp");
   <Body>
 
     <div class="py-6">
-
-          <div class="max-w-5xl py-12 mx-auto sm:px-6 lg:px-8 bg-white overflow-hidden shadow-sm sm:rounded-lg">        
+          <div class="max-w-5xl mx-auto sm:px-6 lg:px-8 bg-white overflow-hidden shadow-sm sm:rounded-lg">        
             <div v-if="user" >
               <div v-if="vars.user.id == 1" >
                 <a :href="route('uslugi.edit', usluga.id)" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Редактировать</a>
               </div>
-            </div>
-            <div class="px-6 text-gray-900">{{ usluga.usl_desc }}</div>           
+            </div>                    
           </div>
+
+          <div class="px-6 text-gray-900 text-center">{{ usluga.usl_desc }}</div>
+
+          <h2 class="my-6 py-6 text-3xl font-semibold text-grey text-center">{{ usluga.preimushestvo1 }}</h2>
+          <h2 class="my-6 py-6 text-3xl font-semibold text-grey text-center">{{ usluga.preimushestvo2 }}</h2>
+          <h2 class="my-6 py-6 text-3xl font-semibold text-grey text-center">{{ usluga.preimushestvo3 }}</h2>
+
+          <div class="px-6 text-gray-900 text-center">{{ usluga.longdescription }}</div>   
         
+          <Address :phone="usluga.phone" :address="usluga.address" />
       
-
-
-      <h1 class="text-4xl font-semibold text-grey text-center py-10"> Юристы в этой категории </h1>
-      <div class="grid grid-cols-3 gap-4 place-content-around px-4"> 
-        
-        <div v-if="firstlawyer" class="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a :href="route('lawyer', firstlawyer.id)">
-                <img class="rounded-t-lg" :src="'/'+firstlawyer.file_path" alt="" />
-            </a>
-            <div class="p-5">
-                <a href="#">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ firstlawyer.name }}</h5>
-                </a>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                  {{firstlawyer.about}}
-                </p>
-                <a :href="route('lawyer', firstlawyer.id)" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Про юриста
-                    <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                </a>
+          <!--lawyers-->
+          <div v-if="lawyers" >
+            <h1 class="text-4xl font-semibold text-grey text-center py-10"> Юристы в этой категории </h1>          
+            <div v-for="item in lawyers">           
+              <div class="w-full md:w-1/3">
+                <div class="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                  <a :href="route('lawyer', item.id)">
+                      <img class="rounded-t-lg" :src="'/'+item.file_path" alt="" />
+                  </a>
+                  <div class="p-5">
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        {{item.about}}
+                      </p>
+                          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ item.name }}</h5>
+                  </div>
+                </div>  
+              </div>
             </div>
-        </div>
+          </div>
+          <!--lawyers-->
 
-        <div v-if="firstlawyer" class=" bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a :href="route('lawyer', firstlawyer.id)">
-                <img class="rounded-t-lg" :src="'/'+firstlawyer.file_path" alt="" />
-            </a>
-            <div class="p-5">
-                <a href="#">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ firstlawyer.name }}</h5>
-                </a>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                  {{firstlawyer.about}}
-                </p>
-                <a :href="route('lawyer', firstlawyer.id)" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Про юриста
-                    <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                </a>
-            </div>
-        </div>
-
-        <div v-if="firstlawyer" class=" bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a :href="route('lawyer', firstlawyer.id)">
-                <img class="rounded-t-lg" :src="'/'+firstlawyer.file_path" alt="" />
-            </a>
-            <div class="p-5">
-                <a href="#">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ firstlawyer.name }}</h5>
-                </a>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                  {{firstlawyer.about}}
-                </p>
-                <a :href="route('lawyer', firstlawyer.id)" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Про юриста
-                    <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                </a>
-            </div>
-        </div>
-
-      </div>
-
-      
+        <Slider :sliderheader="sliderheader" :sldimg="practice" />
       
     </div>
   </Body>
