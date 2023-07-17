7e2996b2a5f3b11f3384abb5fe0c32145ab0acf0
Mark <m6132@yandex.ru>
2023-07-17 17:54:09 +0300

    bug fix

--- app/Http/Controllers/UslugiController.php
+++ app/Http/Controllers/UslugiController.php
@@ -111,12 +111,3 @@ class UslugiController extends Controller
         return redirect()->route('uslugi.url', $url);     
     }    
 }
-
-            $table->text('longdescription')->default('Это детальное описание услуги');
-            $table->text('preimushestvo1')->default('600+ дел');
-            $table->text('preimushestvo2')->default('Более 10 лет практики');
-            $table->text('preimushestvo3')->default('Аналитический подход к решению задачи');
-            $table->text('address')->default('Респ. Крым, г. Симферополь, ул. Долгоруковская, 7');
-            $table->text('phone')->default('+79788838978');
-            $table->text('maps')->default('https://yandex.ru/map-widget/v1/?um=constructor%3Af656829d54430f3c7f23ae0313146d2525f75dfbaffa3e083f51d06d98a33614&amp;source=constructor');
-        
