2db68c5bbd935cae23e9f055ae501ee7b55b4ef8
Mario <you@example.com>
2023-08-08 12:39:51 +0300

    vk

---
+++ resources/js/VKOpenApi.js
@@ -0,0 +1,6 @@
+export default new Promise((res) => {
+    const script = document.createElement('script')
+    script.onload = () => res()
+    script.setAttribute('src', 'https://vk.com/js/api/openapi.js?169')
+    document.head.appendChild(script)
+})
