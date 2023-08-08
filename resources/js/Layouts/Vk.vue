f65dda71f72c2cc6f81cdbbc684e4e6d884f9867
Mario <you@example.com>
2023-08-08 12:41:49 +0300

    vk2

--- resources/js/Layouts/Vk.vue
+++ resources/js/Layouts/Vk.vue
@@ -1,20 +1,31 @@
-<script type="text/javascript" src="https://vk.com/js/api/openapi.js?169"></script>
+<script>
+import { ref } from 'vue'
+import VKOpenApi from '@/VKOpenApi.js'
+
+export default {
+    setup() {
+        const status = ref('loading')
+
+        VKOpenApi.then(() => {
+            status.value = 'Script loaded'
+            VK.Widgets.Group('vk_groups', {mode: 3}, 20003922)
+        })
+
+        return {
+            status
+        }
+    }
+}
+</script>
 
 <template>
-<!-- secondary banner - testimonials -->
-<h1 class="text-4xl font-semibold text-grey text-center py-10">
-    Наши друзья, клиенты и партнеры
-  </h1>
-  <div
-    class="relative overflow-hidden bg-cover bg-no-repeat my-9 p-12 text-center h-96"
+    <!-- secondary banner - testimonials -->
+    <h1 class="text-4xl font-semibold text-grey text-center py-10">Наши друзья, клиенты и партнеры</h1>
+    <div
+        class="relative overflow-hidden bg-cover bg-no-repeat my-9 p-12 text-center h-96"
     >
-        
-
         <!-- VK Widget -->
         <div id="vk_groups"></div>
-            <component :is="'script'">
-                VK.Widgets.Group("vk_groups", {mode: 3}, 20003922);
-            </component>         
-  </div> 
-<!-- secondary banner - testimonials -->
+    </div>
+    <!-- secondary banner - testimonials -->
 </template>
