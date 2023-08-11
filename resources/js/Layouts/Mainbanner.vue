534567a2da6ac79f68acf5c0f6c91bb5946752e7
Mario <you@example.com>
2023-08-11 15:12:05 +0300

    usaluga page

--- resources/js/Layouts/Mainbanner.vue
+++ resources/js/Layouts/Mainbanner.vue
@@ -24,45 +24,53 @@ let submit = () => {
     class="
         relative 
         overflow-hidden 
-        bg-cover 
-        bg-no-repeat 
-        py-12 
-        text-center mb-9"
-    :style="{ backgroundImage: `url(${mainbannerimg})` , height: '600px'}"
+        bg-contain  
+        bg-no-repeat
+        bg-center 
+        w-full
+        h-96
+        "
+        
+    :style="{ backgroundImage: `url(${mainbannerimg})`}"
     >
-    <div
-    class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed"
-    style="background-color: rgba(0, 0, 0, 0.1); opacity: 1;">
+        <div
+            class="grid grid-cols-1 justify-items-end content-center w-full h-full"
+            style="background-color: rgba(0, 0, 0, 0.1); opacity: 1;">
 
-    <div class="h-full md:w-1/2 md:float-right">
-        <div class="flex h-full items-center justify-center">            
-        <div class="text-white">
-            <h2 class="mb-4 text-4xl font-semibold"> {{ statusonimage }}</h2>
-            <h4 class="mb-12 text-2xl font-semibold">{{ nameonimage }}</h4>
-            <h2 class="mt-12 text-3xl font-semibold">Запись</h2>
-            <h4 class="text-xl font-semibold">на консультацию</h4>
-            <div class="">
-            <form @submit.prevent="submit" class="space-y-6">
-                <div>                    
-                    <input v-model="form.phone" type="number" name="phone" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="+7" required>
-                </div>
-                <div class="flex justify-between">
-                    <div class="flex items-start">
-                        <div class="flex items-center h-5">
-                            <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" checked required>
+            <div class="md:w-1/2 md:float-right">
+                <div class="flex  items-center justify-center">            
+                
+                    <div class="text-black text-center">
+                        
+                        <div class="m-5">
+                            <span class="mb-4 text-4xl font-semibold bg-white rounded-lg py-1 px-4">{{ nameonimage }} {{ statusonimage }}</span>
                         </div>
-                        <label for="remember" class="ml-2 text-sm font-medium text-white-900 dark:text-gray-300">Согласен с политикой сайта</label>
+                        <div  class="m-5">
+                            <span class="text-xl font-semibold bg-white rounded-lg py-1 px-4">Запись на консультацию</span>
+                        </div>
+                    
+                        <div class="flex justify-center">
+                            <form @submit.prevent="submit" class="w-80 space-y-6">
+                                <div>                    
+                                    <input v-model="form.phone" type="number" name="phone" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="+7" required>
+                                </div>
+                                <div class="flex justify-between">
+                                    <div class="flex items-start">
+                                        <div class="flex items-center h-5">
+                                            <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" checked required>
+                                        </div>
+                                        <label for="remember" class="ml-2 text-sm font-medium text-white-900 dark:text-gray-300">Согласен с политикой сайта</label>
+                                    </div>
+                                    <a href="/policy" class="text-sm text-blue-700 hover:underline dark:text-blue-500">Подробнее</a>
+                                </div>
+                                <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Отправить</button>
+                            </form>
+                        </div>
                     </div>
-                    <a href="/policy" class="text-sm text-blue-700 hover:underline dark:text-blue-500">Подробнее</a>
-                </div>
-                <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Отправить</button>
-            </form>
-        </div>
 
-        </div>
-        </div>
-    </div>            
-    </div>         
+                </div>
+            </div>            
+        </div>         
     </div>
     <!-- main banner -->
 </template>
--- resources/js/Pages/Uslugi/Usluga.vue
+++ resources/js/Pages/Uslugi/Usluga.vue
@@ -8,7 +8,6 @@ import Body from "@/Layouts/Body.vue";
 import Address from "@/Layouts/Address.vue";
 import MainFooter from "@/Layouts/MainFooter.vue";
 import Slider from "@/Layouts/Slider.vue";
-import { Head, ref } from "@inertiajs/inertia-vue3";
 
 let vars = defineProps({
   usluga: String,
@@ -23,7 +22,7 @@ let sliderheader = "Доверяйте нашей практике";
 let mainbannerimg = '/'+vars.lawyers[0]?.file_path;
 
 if(vars.lawyers[0]?.file_path === undefined){
-    mainbannerimg  = '/storage/images/landing/main/1280on600.webp';
+    mainbannerimg  = 'https://nedicom.ru/storage/images/landing/main/1280on600.webp';
 }
 </script>
 
@@ -54,23 +53,64 @@ if(vars.lawyers[0]?.file_path === undefined){
 
   <Body>
 
-    <div class="py-6">
-          <div class="max-w-5xl mx-auto sm:px-6 lg:px-8 bg-white overflow-hidden shadow-sm sm:rounded-lg">        
-            <div v-if="user" >
-              <div v-if="vars.user.id == 1" >
-                <a :href="route('uslugi.edit', usluga.id)" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Редактировать</a>
-              </div>
-            </div>                    
-          </div>
+          <div class="py-6">
+    <!-- edit btn -->
+            <div class="max-w-5xl mx-auto sm:px-6 lg:px-8 bg-white overflow-hidden shadow-sm sm:rounded-lg">        
+              <div v-if="user" >
+                <div v-if="vars.user.id == 1" >
+                  <a :href="route('uslugi.edit', usluga.id)" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Редактировать</a>
+                </div>
+               </div>                    
+             </div>
+    <!-- edit btn -->
 
-          <div class="px-6 text-gray-900 text-center">{{ usluga.usl_desc }}</div>
+    <!-- header 2 -->
+          <div class="text-4xl m-12 px-6 font-semibold text-grey text-center">{{usluga.usl_name}}</div>
+    <!-- header 2 -->
 
-          <h2 class="my-6 py-6 text-3xl font-semibold text-grey text-center">{{ usluga.preimushestvo1 }}</h2>
-          <h2 class="my-6 py-6 text-3xl font-semibold text-grey text-center">{{ usluga.preimushestvo2 }}</h2>
-          <h2 class="my-6 py-6 text-3xl font-semibold text-grey text-center">{{ usluga.preimushestvo3 }}</h2>
+    <!-- short desc -->
+          <div class="mx-12 px-6 text-gray-900 text-center">{{ usluga.usl_desc }}</div>
+    <!-- short desc -->
+
+    <!-- preimushestva -->
+    <div class="my-12">
+      <div class="m-6 flex justify-center">
+          <div class="">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="fill-indigo-500 hover:fill-indigo-600 bi bi-file-earmark-check" viewBox="0 0 16 16">
+            <path d="M10.854 7.854a.5.5 0 0 0-.708-.708L7.5 9.793 6.354 8.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"/>
+            <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
+          </svg>
+          </div> 
+      </div>
+
+      <h2 class="py-3 text-3xl font-semibold text-grey text-center">{{ usluga.preimushestvo1 }}</h2>
+            
+      <div class="m-6 flex justify-center">
+          <div class="">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="fill-indigo-500 hover:fill-indigo-600 bi bi-bar-chart-line" viewBox="0 0 16 16">
+          <path d="M11 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h1V7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7h1V2zm1 12h2V2h-2v12zm-3 0V7H7v7h2zm-5 0v-3H2v3h2z"/>
+        </svg>
+          </div> 
+      </div>
+      
+      <h2 class="py-3 text-3xl font-semibold text-grey text-center">{{ usluga.preimushestvo2 }}</h2>
+            
+      <div class="m-6 flex justify-center">
+          <div class="">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="fill-indigo-500 hover:fill-indigo-600 bi bi-hand-index" viewBox="0 0 16 16">
+              <path d="M6.75 1a.75.75 0 0 1 .75.75V8a.5.5 0 0 0 1 0V5.467l.086-.004c.317-.012.637-.008.816.027.134.027.294.096.448.182.077.042.15.147.15.314V8a.5.5 0 1 0 1 0V6.435a4.9 4.9 0 0 1 .106-.01c.316-.024.584-.01.708.04.118.046.3.207.486.43.081.096.15.19.2.259V8.5a.5.5 0 0 0 1 0v-1h.342a1 1 0 0 1 .995 1.1l-.271 2.715a2.5 2.5 0 0 1-.317.991l-1.395 2.442a.5.5 0 0 1-.434.252H6.035a.5.5 0 0 1-.416-.223l-1.433-2.15a1.5 1.5 0 0 1-.243-.666l-.345-3.105a.5.5 0 0 1 .399-.546L5 8.11V9a.5.5 0 0 0 1 0V1.75A.75.75 0 0 1 6.75 1zM8.5 4.466V1.75a1.75 1.75 0 1 0-3.5 0v5.34l-1.2.24a1.5 1.5 0 0 0-1.196 1.636l.345 3.106a2.5 2.5 0 0 0 .405 1.11l1.433 2.15A1.5 1.5 0 0 0 6.035 16h6.385a1.5 1.5 0 0 0 1.302-.756l1.395-2.441a3.5 3.5 0 0 0 .444-1.389l.271-2.715a2 2 0 0 0-1.99-2.199h-.581a5.114 5.114 0 0 0-.195-.248c-.191-.229-.51-.568-.88-.716-.364-.146-.846-.132-1.158-.108l-.132.012a1.26 1.26 0 0 0-.56-.642 2.632 2.632 0 0 0-.738-.288c-.31-.062-.739-.058-1.05-.046l-.048.002zm2.094 2.025z"/>
+            </svg>
+          </div> 
+      </div>
+    
+      <h2 class="py-3 text-3xl font-semibold text-grey text-center">{{ usluga.preimushestvo3 }}</h2>
+    </div>
+    <!-- preimushestva -->
 
           <div class="px-6 text-gray-900 text-center">{{ usluga.longdescription }}</div>   
         
+          <Slider v-if="practice != 0" :sliderheader="sliderheader" :sldimg="practice" />
+      
           <Address :phone="usluga.phone" :address="usluga.address" />
 
           <!--lawyers-->
@@ -100,8 +140,6 @@ if(vars.lawyers[0]?.file_path === undefined){
           </div>
           <!--lawyers-->
 
-        <Slider v-if="practice != 0" :sliderheader="sliderheader" :sldimg="practice" />
-      
     </div>
   </Body>
 
