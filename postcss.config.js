b94bcf8adbca30dfde70606c6a8e272403a25d97
Mark Local <m6132@yandex.ru>
2023-10-21 10:50:58 +0300

    tailwind nested

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    plugins: {
+    plugins: {        
+        'postcss-import': {},
+        'tailwindcss/nesting': 'postcss-nesting',
         tailwindcss: {},
         autoprefixer: {},
     },
