776b4d6ac0fdec965defd4b385966435007b3b54
Mark Local <m6132@yandex.ru>
2023-10-21 13:45:01 +0300

    captcheKey

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,7 +9,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 import { createVfm } from 'vue-final-modal';
 import { Inertia } from '@inertiajs/inertia';
-import { VueReCaptcha } from 'vue-recaptcha-v3';
+import { VueReCaptcha, useReCaptcha  } from 'vue-recaptcha-v3';
 import 'flowbite';
 
 const vfm = createVfm();
@@ -18,10 +18,10 @@ createInertiaApp({
     title: (title) => `${title}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, app, props, plugin }) {
-        //const captcheKey = props.initialPage.props.recaptcha_site_key;
+        const captcheKey = props.initialPage.props.recaptcha_site_key;
         return createApp({ render: () => h(app, props) })
             .use(plugin)
-            .use(VueReCaptcha, { siteKey: '<6Lf0-tAZAAAAAIxKP1YOtKrCfqSm_yl3QF-IzglK>' } )
+            .use(VueReCaptcha, { siteKey: captcheKey } )
             .use(vfm)
             .use(InertiaLinkInertiaLink)
             .use(ZiggyVue, Ziggy)
