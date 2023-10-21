6f2a462e6bab86290301f305e2e64c257b429733
Mark Local <m6132@yandex.ru>
2023-10-21 15:33:56 +0300

    url

--- config/services.php
+++ config/services.php
@@ -32,7 +32,7 @@ return [
     ],
 
     'google_recaptcha' => [
-        'url' => 'https://www.google.com/recaptcha/api/siteverify',
+        'url' => 'https://www.google.com/recaptcha/api.js?onload=loadCaptcha&render=explicit',
         'site_key' => env('GOOGLE_RECAPTCHA_SITE_KEY'),
         'secret_key' => env('GOOGLE_RECAPTCHA_SECRET_SITE_KEY'),
       ]
