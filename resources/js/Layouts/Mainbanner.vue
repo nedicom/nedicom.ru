802d5e8c8eef6bbcd89588520d18e81930856269
Mark Local <m6132@yandex.ru>
2023-10-21 15:29:51 +0300

    recaptcha

--- resources/js/Layouts/Mainbanner.vue
+++ resources/js/Layouts/Mainbanner.vue
@@ -77,7 +77,7 @@ let submit = () => {
                                     <a href="/policy" class="text-sm bg-white px-1 rounded-lg text-blue-700 hover:underline dark:text-blue-500">Подробнее</a>
                                 </div>
 
-                                <button type="submit" 
+                                <button type="submit"  id="recaptcha"
                                     class="g-recaptcha w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Отправить</button>                               
                             </form>
                         </div>
