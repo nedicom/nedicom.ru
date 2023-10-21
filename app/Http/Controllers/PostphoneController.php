80ad6475a7f07f237ef7ad92f27fd4bc9ed2bfb3
Mark Local <m6132@yandex.ru>
2023-10-21 21:22:28 +0300

    dev

--- app/Http/Controllers/PostphoneController.php
+++ app/Http/Controllers/PostphoneController.php
@@ -14,25 +14,8 @@ class PostphoneController extends Controller
         
             $endpoint = config('services.google_recaptcha');
 
-            /*$response = Http::asForm()->post($endpoint['url'], [
-                'secret' => $endpoint['secret_key'],
-                'response' => $request->token,           
-            ]);
-            
-    
-            //if(  $response['success'] && $response['score'] > 0.5) {
-            if(  $response['success'] ) {
-                dd('test');
-            }*/
-
             $post_data = "secret=6Lf0-tAZAAAAACjG_OHu4hkZxvj92Q6kuxfvCZiY"."&response=".$request->token;
 
-            /*$post_data = array(
-                'secret' => "6Lf0-tAZAAAAACjG_OHu4hkZxvj92Q6kuxfvCZiY",
-                'response' => $request->token,
-            );
-            $post_data = json_encode($post_data);
-*/
             $ch = curl_init(); 
             curl_setopt($ch, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
             curl_setopt($ch, CURLOPT_POST, true);
@@ -43,30 +26,23 @@ class PostphoneController extends Controller
             $decgoogresp = json_decode($googresp);
             curl_close($ch);
 
-            dd($post_data, $googresp, $decgoogresp);
+            //dd($post_data, $googresp, $decgoogresp);
             if ($decgoogresp->success == true)
                 {
-                    dd('good');
+                    $phone = $request->phone;
+                    $conn = mysqli_connect("178.208.94.106", "crm", "904klfkFL:DlflrD4", "crm");
+                        if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                        } 
+    
+                    $sql = "INSERT INTO leads (source, description, phone, lawyer, created_at, responsible, status, service)
+                    VALUES ('nedicom.ru', 'лид с главной nedicom.ru', $phone, 2, CURRENT_TIME(), 2, 'поступил', 5)"; //2 - Mark, 4 - Анастасия, 5 - иск, 67 - вера
+                    $conn->query($sql);
+                    return redirect()->back()->with('success', 'Ваш телефон отправлен. Скоро мы Вам перезвоним!');
                 }
                 else
                 {
-                    dd('bad');
+                    return redirect()->back()->with('success', 'Не прошла капча. Попробуйте отправить телефон еще раз.');
                 }
-            
-
-        
-        
-                
-                $phone = $request->phone;
-                $conn = mysqli_connect("178.208.94.106", "crm", "904klfkFL:DlflrD4", "crm");
-                    if ($conn->connect_error) {
-                    die("Connection failed: " . $conn->connect_error);
-                    } 
-
-                $sql = "INSERT INTO leads (source, description, phone, lawyer, created_at, responsible, status, service)
-                VALUES ('nedicom.ru', 'лид с главной nedicom.ru', $phone, 2, CURRENT_TIME(), 2, 'поступил', 5)"; //2 - Mark, 4 - Анастасия, 5 - иск, 67 - вера
-                //$conn->query($sql);
-
-        return redirect()->back()->with('success', 'Ваш телефон отправлен. Скоро мы Вам перезвоним!');
     }
 }
