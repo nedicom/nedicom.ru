89760e73e73e36194ac112a4bd3c40fd046e6ccf
Mark Local <m6132@yandex.ru>
2023-10-22 21:40:27 +0300

    finish

--- resources/js/Layouts/Mainbanner.vue
+++ resources/js/Layouts/Mainbanner.vue
@@ -75,7 +75,7 @@ const recaptcha = async () => {
                                     <a href="/policy" class="text-sm bg-white px-1 rounded-lg text-blue-700 hover:underline dark:text-blue-500">Подробнее</a>
                                 </div>
 
-                                <button type="submit"  
+                                <button type="submit"   
                                     class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Отправить</button>                               
                                     <div class="text-red-500 bg-white rounded-lg px-2 py-1 font-semibold text-sm animate-spin-pulse" v-if="$page.props.errors['token']">{{ $page.props.errors['token'] }}</div>
                                 </form>
