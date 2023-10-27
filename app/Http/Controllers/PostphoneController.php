cd41d24840d2ff8b8c41dd50218bb2137be20b4a
Mark Local <m6132@yandex.ru>
2023-10-27 21:34:00 +0300

    test 2

--- app/Http/Controllers/PostphoneController.php
+++ app/Http/Controllers/PostphoneController.php
@@ -25,18 +25,15 @@ use App\Rules\ReCaptcha;
                     
                     //if(!config('app.debug'))  ! - for development
                                  
-                                   $phone = $request->phone;
-                                    $conn = mysqli_connect("localhost", "crm", "904klfkFL:DlflrD4", "crm");
-                                        if ($conn->connect_error) {
-                                        die("Connection failed: " . $conn->connect_error);
-                                        } 
-                    
-                                    $sql = "INSERT INTO leads (source, description, phone, lawyer, created_at, responsible, status, service)
-                                    VALUES ('nedicom.ru', 'лид с ".$request->url."', $phone, 2, CURRENT_TIME(), 2, 'поступил', 5)"; //2 - Mark, 4 - Анастасия, 5 - иск, 67 - вера
-                                    $conn->query($sql);
-                                    return redirect()->back()->with('message', $request->token);
-                        
-
-                    return redirect()->back()->with('message', $request->phone);
+                    $phone = $request->phone;
+                    $conn = mysqli_connect("localhost", "crm", "904klfkFL:DlflrD4", "crm");
+                        if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                        } 
+    
+                    $sql = "INSERT INTO leads (source, description, phone, lawyer, created_at, responsible, status, service)
+                    VALUES ('nedicom.ru', 'лид с ".$request->url."', $phone, 2, CURRENT_TIME(), 2, 'поступил', 5)"; //2 - Mark, 4 - Анастасия, 5 - иск, 67 - вера
+                    $conn->query($sql);
+                    return redirect()->back()->with('message', 'Ваш телефон успешно отправлен! Скоро мы свяжемся с Вами.');                        
                 }       
         }
