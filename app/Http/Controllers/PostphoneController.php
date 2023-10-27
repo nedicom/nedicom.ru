db1266010fbed8986062227ff4918d0d2de13c39
Mark Local <m6132@yandex.ru>
2023-10-27 21:31:27 +0300

    test

--- app/Http/Controllers/PostphoneController.php
+++ app/Http/Controllers/PostphoneController.php
@@ -24,7 +24,7 @@ use App\Rules\ReCaptcha;
                     );       
                     
                     //if(!config('app.debug'))  ! - for development
-                                 /* 
+                                 
                                    $phone = $request->phone;
                                     $conn = mysqli_connect("localhost", "crm", "904klfkFL:DlflrD4", "crm");
                                         if ($conn->connect_error) {
@@ -35,7 +35,7 @@ use App\Rules\ReCaptcha;
                                     VALUES ('nedicom.ru', 'лид с ".$request->url."', $phone, 2, CURRENT_TIME(), 2, 'поступил', 5)"; //2 - Mark, 4 - Анастасия, 5 - иск, 67 - вера
                                     $conn->query($sql);
                                     return redirect()->back()->with('message', $request->token);
-                        */
+                        
 
                     return redirect()->back()->with('message', $request->phone);
                 }       
