839ddde140cdf1ec42c71a8da27a680967ecd94c
Mark Local <m6132@yandex.ru>
2023-12-04 20:59:34 +0300

    useravatar

--- resources/js/Components/Question.vue
+++ resources/js/Components/Question.vue
@@ -4,20 +4,19 @@ import PrimaryButton from '@/Components/PrimaryButton.vue';
 
 
 <template>
-    <main class="pt-16  bg-white dark:bg-gray-900 antialiased">
+    <main class="pt-16 xl:mt-16 bg-white dark:bg-gray-900 antialiased">
     <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
         <article class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
             <header class="mb-8 lg:mb-6 not-format">
 
 
-                <div class="flex items-center">
-                <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold"><img
-                        class="mr-2 w-6 h-6 rounded-full"
-                        src="/storage/usr/1/avatar/1701703350avatar.webp"
-                        alt="Незарегистрированный пользователь">Вы</p>
-                <p class="text-sm text-gray-600 dark:text-gray-400"><time pubdate datetime="2022-02-08"
-                        title="February 8th, 2022">Только что</time></p>
-            </div>
+                <div class="flex items-center mb-5">
+                    <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold"><img
+                            class="mr-2 w-6 h-6 rounded-full"
+                            src="/storage/default/user.webp"
+                            alt="Незарегистрированный пользователь">Вы</p>
+                    <p class="text-sm text-gray-600 dark:text-gray-400"><time>Только что</time></p>
+                </div>
 
                 <h1 class="mb-4 text-2xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
                     <slot name="title"></slot>
