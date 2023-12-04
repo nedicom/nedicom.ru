609d80d085b3a887aac7364bdfaa83bb6dec2559
Mark Local <m6132@yandex.ru>
2023-12-04 18:49:49 +0300

    radio

--- database/migrations/2023_08_23_105311_avatar.php
+++ database/migrations/2023_08_23_105311_avatar.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('avatar_path')->default('/storage/default/avatar.webp');
+            $table->string('avatar_path')->default('storage/default/avatar.webp');
         });
     }
 
--- resources/js/Components/RadioLawyer.vue
+++ resources/js/Components/RadioLawyer.vue
@@ -8,7 +8,7 @@ defineProps({
     <p class="mb-5 text-2xl font-semibold tracking-tight text-gray-900">Выбрать юриста для ответа</p>
     <label v-for="value in lawyers" :key="value" class="group flex item-center mb-2">
         <input type="radio" :value="value.id" name="radiolawyer" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-        <img :src='"/"+value.avatar_path' width="40" class="rounded-full">
+        <img :src='"/"+value.avatar_path ' width="40" class="rounded-full">
         <div class="flex items-center justify-center px-1 text-sm labeldiv">{{value.name}}</div>
     </label>
 </template>
